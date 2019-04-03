2d0635bc475a0629ca1af4063cd1cddc52586bc1
Lunny Xiao <xiaolunwen@gmail.com>
2019-04-03 22:08:31 +0800

    add database migrations and fix app.ini sample

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -227,6 +227,8 @@ var migrations = []Migration{
 	NewMigration("hash application token", hashAppToken),
 	// v86 -> v87
 	NewMigration("add http method to webhook", addHTTPMethodToWebhook),
+	// v87 -> v88
+	NewMigration("add task table and status column for repository table", addTaskTable),
 }
 
 // Migrate database to current version
---
+++ models/migrations/v87.go
@@ -0,0 +1,33 @@
+// Copyright 2019 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package migrations
+
+import (
+	"code.gitea.io/gitea/modules/structs"
+	"code.gitea.io/gitea/modules/util"
+	"github.com/go-xorm/xorm"
+)
+
+func addTaskTable(x *xorm.Engine) error {
+	type Task struct {
+		ID             int64
+		DoerID         int64 `xorm:"index"` // operator
+		OwnerID        int64 `xorm:"index"` // repo owner id, when creating, the repoID maybe zero
+		RepoID         int64 `xorm:"index"`
+		Type           structs.TaskType
+		Status         structs.TaskStatus `xorm:"index"`
+		StartTime      util.TimeStamp
+		EndTime        util.TimeStamp
+		PayloadContent string         `xorm:"TEXT"`
+		Errors         string         `xorm:"TEXT"` // if task failed, saved the error reason
+		Created        util.TimeStamp `xorm:"created"`
+	}
+
+	type Repository struct {
+		Status int
+	}
+
+	return x.Sync2(new(Task), new(Repository))
+}
